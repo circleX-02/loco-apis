3ccffcb3ae5a5bd07aa6c6e1287da54e8364488e
OcircleX <circlex02@gmail.com>
2025-08-19 14:32:03 +0500

    changes

--- controllers/busController.js
+++ controllers/busController.js
@@ -2,15 +2,20 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const Category = require('../models/Category'); // import category model
 
 const business = async (req, res) => {
   try {
     // Fetch all users except those with role: "admin", excluding password and __v
     const allUsers = await User.find({ role: { $ne: 'admin' } }).select('-password -__v');
+    const allCategories = await Category.find().select('-__v');
+
     return res.status(200).json({
       success: true,
       message: 'Data retrieved',
       users: allUsers, // Return non-admin users
+      categories: allCategories   // 👈 include categories here
+
     });
   } catch (error) {
     console.error('Data Error:', error);
